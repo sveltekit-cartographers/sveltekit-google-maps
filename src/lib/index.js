f3a4df825c98ae9ce408c1e95aafc441bd37a6f1
fawaz-alesayi <fawaztsa@gmail.com>
2023-03-11 15:38:44 +0300

    initial map component 🗺

---
+++ src/lib/index.js
@@ -0,0 +1,2 @@
+// Reexport your entry components here
+export { default as Map } from '$lib/components/Map.svelte';
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-hamlindigo.css';
+	
+	import '@skeletonlabs/skeleton/styles/all.css';
+
+	import '../app.postcss';
+</script>
+
+<slot />
